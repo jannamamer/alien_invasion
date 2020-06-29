9227a0f6a841427e698845ddb6250f34f304afb0
Janna Dela Cruz <53164350+jannamamer@users.noreply.github.com>
2020-06-30 11:34:11 +1200

    Chapter 12: Adding firing of bullets

--- bullet.py
+++ bullet.py
@@ -8,7 +8,7 @@ class Bullet(Sprite):
 
     def __init__(self, ai_game):
         """Create a bullet object at the ship's current position."""
-        super().__init()
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
@@ -28,6 +28,6 @@ class Bullet(Sprite):
         # Update the rect position.
         self.rect.y = self.y
 
-    def draw(self):
+    def draw_bullet(self):
         """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
